b848e5764df3f9f19b4e3ff1a76f077201011d9f
David Stevens <dstevens2712@gmial.com>
2022-05-08 16:22:51 -0400

    updates to models and views files.

--- qq/quotes/models.py
+++ qq/quotes/models.py
@@ -1,45 +1,36 @@
 from django.db import models
 
-
-
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length = 1000)
+     
 class Quote(models.Model):
-    text = models.CharField(max_length=1000)
-    meme = models.ImageField(upload_to='images', default='')
-    tags = models.ManyToManyField('Tag')
+    text = models.TextField()
     category = models.ManyToManyField('Category')
-    author = models.ForeignKey('Author', on_delete = models.CASCADE, default='')
-
-
+    #meme = models.ImageField(upload_to='images', default='')
+    #tags = models.ManyToManyField('Tag')
+'''
     def __str__(self):
         if self.text:
             return self.text        
         return self.meme
+'''
 
 
 class Author(models.Model):
-    author = models.CharField(max_length = 1000)
-    
-    def _str_(self):
-       return self.author 
+    author = models.CharField(max_length = 500)
+    quote = models.ForeignKey(Quote, on_delete = models.CASCADE, default='Anonymous', null=True, blank=True)
 
+
+
+
+'''
 class Tag(models.Model):
     tag = models.CharField(max_length = 1000)
     
     def _str_(self):
        return self.tag 
-
-class Category(models.Model):
-    category = models.CharField(max_length = 1000)
-    
-    def _str_(self):
-       return self.category 
-  
-
-
-  
-
-
-
+pass
+'''
--- qq/quotes/views.py
+++ qq/quotes/views.py
@@ -1,45 +1,98 @@
 from django.shortcuts import render
 from django.views import View
 
-from quotes.forms import SearchForm, NavForm
+from quotes.forms import SearchForm, NavBar
 from quotes.models import Home, About_us, Results
 # Create your views here.
 class Home(View):           #Get the Home page for Quotable Quotes
     def get(self, request):
         search_form = SearchForm  #Used to seardh database for Quotes
-        nav_form = NavForm        #Used on each page for quickly finding Quote using a tag or comment
-        
+         
         return render (request = request,
         template_name = 'index.html',
         context={
-            #if needed ???
+            'search_form' : search_form
+                        #if needed ???
         },
         )
 
-class About_us(View):
+    def post(self, request):
+        form = SearchForm(request.POST)
+        form.save()
+
+        return redirect('results')
+
+
+'''
+class AboutUs(View):
     def get(self, request):
-        nav_from = NavForm
 
         return render(request, template_name = 'about')
-  
+''' #Do we need this class?????
+
+
+
+class AddQuote(View):
+    def get(self, request, quote_id):
+        quote_object = Quote.objects.get(id=quote_id)
+        quote_form =QuoteForm(instance=quote_object)
+
+        authors = Author.objects.filter(author=author_object)
+        AuthorForm(author=author_object)
+
+        return render( 
+            request=request,
+            template_name='add_quote.html',
+            context={
+                'quote_form' : quote_form,
+                'id' : quote_id,
+            }
+        )       
+    def post(self, request, quote_id):
+        quote = Quote.objects.get(id=quote_id)
+
+        if 'save_quote' in request.POST:
+            quote_form = QuoteForm(request.POST, instance=quote)
+            quote_form.save()
+
+        elif 'delete_quote' in request.POST:
+            author.delete()
+
+        elif 'save_author' in request.POST:
+            # The user clicked the button to add a comment, not update the quote
+            author_form = AuthorForm(request.POST, author=author)
+            author_form.save()
+
 
 class Results(View):
-    def get(self, request):
-        nav_form = NavForm
+    def get(self, request, quote_id, author_id):
     
-        return render(request, template_name = 'results')
 
 
-class Add_Quote(View):
-    def get(self, request):
-        pass
+        quote_object = Quote.objects.get(id=quote_id)
+        quote_form = QuoteForm(quote=quote_object)
+#Do we nead an instance ??????????????????????????????????????
+        authors = quote_object.authors.all()
+        author_form = AuthorForm(author=author_object)  # Tag.objects.filter(task=task)
+        
+       
+        return render(
+        request=request,
+        template_name='results.html',
+        context={
+            'quote_form': quote_form,
+            'id': quote_id,
+            'author_form': author_form,
+        }
+    )
+
+
+
 
 
 class Advance_search(View):
     pass
 
 
-class Result(View):
-    pass
 
 

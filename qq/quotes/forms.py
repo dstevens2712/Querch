ca5f43a22bd8c80cebc1f63283979abdf2078b75
1mfarhi <manuelafarhi@sbcglobal.net>
2022-05-09 14:54:06 -0500

    changed forms, models, templates, views

---
+++ qq/quotes/forms.py
@@ -0,0 +1,49 @@
+from asyncio import Task
+from unicodedata import category
+from django import forms
+from .models import Quote, Author, Category, Tag 
+
+
+class QuoteForm(forms.ModelForm):
+    class Meta:
+        model = Quote
+        fields = ['text', 'author']
+
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author', None)
+        super().__init__(*args, **kwargs)
+        self.fields['text'].label = 'Quote'
+        self.fields['author'].label = 'Author'
+        self.instance.author = self.author
+
+    def save(self, *args, **kwargs):
+        author, create = Author.objects.get_or_create(author=self.data['author'])
+        self.quote.author.add(author)
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        self.quote = kwargs.pop('quote', None)
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = 'Category'
+        self.instance.category = self.category
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['tag']
+    def __init__(self, *args, **kwargs):
+        self.quote = kwargs.pop('quote', None)
+        super().__init__(*args, **kwargs)
+
+        self.instance.quote = self.quote
+        self.fields['tag'].label = 'Tag'
+
+    def save(self, *args, **kwargs):
+
+        tag, create = Tag.objects.get_or_create(tag=self.data['tag'])
+        self.quote.tags.add(tag)
+        
--- qq/quotes/models.py
+++ qq/quotes/models.py
@@ -7,35 +7,36 @@ from django.db import models
 
 class Quote(models.Model):
     text = models.CharField(max_length=1000)
-    meme = models.ImageField(upload_to='images', default='')
+    # meme = models.ImageField(upload_to='images', default='')
     tags = models.ManyToManyField('Tag')
     category = models.ManyToManyField('Category')
-    author = models.ForeignKey('Author', on_delete = models.CASCADE, default='')
+    author = models.ForeignKey('Author', on_delete = models.CASCADE, default='Anonymous', null=True, blank=True)
 
 
     def __str__(self):
         if self.text:
             return self.text        
-        return self.meme
+        # return self.meme
 
 
 class Author(models.Model):
-    author = models.CharField(max_length = 1000)
+    author = models.CharField(max_length = 250)
     
-    def _str_(self):
+    def __str__(self):
        return self.author 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length = 1000)
+    tag = models.CharField(max_length = 250)
     
-    def _str_(self):
+    def __str__(self):
        return self.tag 
 
 class Category(models.Model):
-    category = models.CharField(max_length = 1000)
+    name = models.CharField(max_length = 250)
     
-    def _str_(self):
-       return self.category 
+    def __str__(self):
+       return self.name 
+
   
 
 

e7b01a5c8179edfbacfa7ce410ecec0afcaba3ad
Dave218 <dvk5775@yahoo.com>
2022-05-16 19:09:14 -0500

    updated the home page and both results pages views and the urls

--- qq/quotes/models.py
+++ qq/quotes/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-import csv
+
 
 # Create your models here.
 
 class Quote(models.Model):
 
     text = models.CharField(max_length=1000)
-    tags = models.ManyToManyField('Tag', null=True, blank=True)
-    category = models.ManyToManyField('Category', null=True, blank=True)
+    tags = models.ManyToManyField('Tag')
+    category = models.ManyToManyField('Category')
     author = models.ForeignKey('Author', on_delete = models.CASCADE, default='Anonymous', null=True, blank=True)
 
     def __str__(self):
@@ -31,10 +31,10 @@ class Tag(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length = 250, null=True, blank=True)
+    category = models.CharField(max_length = 250, null=True, blank=True)
     
     def __str__(self):
-       return self.name 
+       return self.category 
 
   
 
--- qq/quotes/urls.py
+++ qq/quotes/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Home, AboutUs, AddQuote
+from .views import Home, AboutUs, AddQuote, Results, Result
 
 urlpatterns = [
     path('', Home.as_view(), name = 'home'),
     path('about', AboutUs.as_view(), name='about'),
-    #path('add', AddQuote.as_view(), name='add'),
+    path('add', AddQuote.as_view(), name='add'),
+    path('results', Results.as_view(), name='results'),
+    path('result/<str:quote_id>', Result.as_view(), name='result'),
 ]

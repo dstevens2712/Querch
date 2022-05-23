b210b03978fed846cee7444f387ed0a027c15dfc
Dave218 <dvk5775@yahoo.com>
2022-05-23 18:07:11 -0500

    finalized and working site

--- qq/quotes/urls.py
+++ qq/quotes/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Home, AboutUs, AddQuote, Results, Result
+from .views import Home, AboutUs, AddQuote, Results, Result, Update
 
 urlpatterns = [
-    path('', Home.as_view(), name = 'home'),
+    path('', Home.as_view(), name='home'),
     path('about', AboutUs.as_view(), name='about'),
     path('add', AddQuote.as_view(), name='add'),
     path('results', Results.as_view(), name='results'),
-    path('result/<str:quote_id>', Result.as_view(), name='result'),
+    path('result/<int:quote_id>', Result.as_view(), name='result'),
+    path('update/<int:quote_id>', Update.as_view(), name='update'),
 ]
+

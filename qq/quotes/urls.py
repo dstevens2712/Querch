38b23bef601d6b5189e094c570891a17c4e11963
TaraMay21 <tmpuff21@gmail.com>
2022-06-29 13:02:25 -0600

    search bar

--- qq/quotes/urls.py
+++ qq/quotes/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Home, AboutUs, AddQuote, Results, Result, Update
+from .views import Home, AboutUs, AddQuote, Results, Result, Update, Search
 
 # connects our code to a specific page
 # code will render the following URLs: /home /about /add /results
@@ -13,5 +13,6 @@ urlpatterns = [
     path('results', Results.as_view(), name='results'),
     path('result/<int:quote_id>', Result.as_view(), name='result'),
     path('update/<int:quote_id>', Update.as_view(), name='update'),
+    path("search/", Search.as_view(), name='search')
 ]
 

c58cada07486bd07898ac599da37c42c6c87d0ec
TaraMay21 <tmpuff21@gmail.com>
2022-06-30 18:26:08 -0600

    urls

--- qq/qq/urls.py
+++ qq/qq/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
+    
 ]
 

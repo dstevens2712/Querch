28f392086a9aa23cd549d57d253debead5092710
TaraMay21 <tmpuff21@gmail.com>
2022-06-30 18:22:13 -0600

    quotes

--- qq/quotes/forms.py
+++ qq/quotes/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .models import Quote, Author, Category, Tag, Person
-from django.forms import ModelForm
+from .models import Quote, Author, Category, Tag
+#from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -52,14 +52,7 @@ class TagForm(forms.ModelForm):
         self.quote.tags.add(tag)
 
 
-class RegisterForm(ModelForm):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=PasswordInput)
-    email = forms.CharField(widget=EmailInput)
 
-    class Meta:
-        model = User
-        fields = ["username", "password1", "password2", "email"]
 
 
         

4ed6019f64f32d80c7ec8396a7b20c4912903f79
1mfarhi <manuelafarhi@sbcglobal.net>
2022-07-03 13:31:18 -0500

    delete register.html

--- qq/quotes/views.py
+++ qq/quotes/views.py
@@ -169,7 +169,6 @@ class Update(View):
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "signup.html"
-
+    template_name = "registration/signup.html"
     
 
